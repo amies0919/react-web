2b69157deb3ded21c998865be04685b02015f750
amies <amies.shen@gmail.com>
2017-05-08 03:24:06 -0700

    add demos

--- app/index.js
+++ app/index.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => (
-    <div>Hello , {props.name} !!!</div>
-  );
+const MyList = (props) => (
+    <ul>
+      {
+        props.items.map((item) => (
+            <li key = {item.id}>{item.text}</li>
+          ))
+      }
+    </ul>
+  )
 
-App.propTypes = {
-  name: React.PropTypes.string,
-}  
+ class App extends React.Component{
+    constructor(props) {
+      super(props);
+      this.onChange = this.onChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.state = {
+          items: [],
+          text: ''
+      }
+    }
+    onChange(e) {
+      this.setState({text: e.target.value})
+    }
+    handleSubmit(e) {
+      e.preventDefault();
+      const nextItems = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
+      const nextText = '';
+      this.setState({items: nextItems, text: nextText});
+    }
+    render() {
+      return (
+            <div>
+              <h3>TODO</h3>
+              <MyList items = {this.state.items} />
+              <form onSubmit = {this.handleSubmit}>
+                  <input onChange = {this.onChange} value={this.state.text} />
+                  <button>{'Add #' + (this.state.items.length + 1)}</button>
+              </form>
+            </div>
+        )
+    }
+ } 
 
-App.defaultProps = {
-  name: 'Zuck',
-}
 
 ReactDOM.render(<App />, document.getElementById('app'));
